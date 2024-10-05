85e6fa9d5090a9338f364ee98c1df63c1e24ab4b
Aswin-Foxiom <aswinfoxiom@gmail.com>
2024-10-05 13:31:25 +0530

    code updated

--- app/utils/BaseUrl.jsx
+++ app/utils/BaseUrl.jsx
@@ -1,2 +1,2 @@
-// export const BaseUrl = "http://192.168.1.48:3001";
-export const BaseUrl = "https://server.joonms.com";
+export const BaseUrl = "http://192.168.1.50:3001";
+// export const BaseUrl = "https://server.joonms.com";
