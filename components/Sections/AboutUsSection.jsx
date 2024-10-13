76d6523ece803d0ed35901ab69c56437797e9c5d
Aswin-Foxiom <aswinfoxiom@gmail.com>
2024-10-13 11:12:48 +0530

    about section updated

--- components/Sections/AboutUsSection.jsx
+++ components/Sections/AboutUsSection.jsx
@@ -31,13 +31,11 @@ function AboutUsSection() {
                 </div> */}
               </div>
             </div>
-            <div className="content-column col-lg-6 col-md-12 col-sm-12">
+            {/* <div className="content-column col-lg-6 col-md-12 col-sm-12">
               <div className="inner-column">
                 <div className="sec-title">
                   <h2>About Us</h2>
-                  {/* <div className="text">
-                    Leading Digital Agency for <br /> Business Solution.
-                  </div> */}
+                 
                 </div>
                 <div className="text">
                   <p>
@@ -63,9 +61,93 @@ function AboutUsSection() {
                 <div className="author">
                   Baijoon N S <br /> ( Administrative Coordinator )
                 </div>
-                {/* <div className="signature">
-                  <img src="images/icons/signature.png" alt="" />
-                </div> */}
+          
+              </div>
+            </div> */}
+            <div className="carousel-column col-lg-6 col-md-12 col-sm-12">
+              <div className="inner-column">
+                <div className="sec-title">
+                  <h2>About Us</h2>
+                  {/* <div className="text">
+                    Check what&rsquo;s our client say about us!
+                  </div> */}
+                </div>
+                <div className="testimonial-carousel owl-carousel owl-theme">
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      {/* <div className="quote-icon flaticon-quote-2" /> */}
+                      <div className="text">
+                        JoonMS is a dynamic hardware distributor and software
+                        developer, proudly headquartered in Dubai and operating
+                        throughout the GCC countries. Our real estate and
+                        property management software has gained widespread
+                        popularity in a short time, and we are expanding our
+                        Real Estate Operations Management (REOM) and Property
+                        Operations Management (POM) solutions. Built on the
+                        fundamentals of CaFM, we are developing these systems
+                        into a comprehensive software suite that will serve as a
+                        full replacement for traditional Enterprise Resource
+                        Planning (ERP) systems, offering businesses a smarter,
+                        more efficient alternative.
+                      </div>
+                      {/* <div className="author">- Raisa Moni</div>
+                      <div className="designation">Envato Customer</div> */}
+                    </div>
+                  </div>
+
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      {/* <div className="quote-icon flaticon-quote-2" /> */}
+                      <div className="text">
+                        JoonMS is the Management Ecosystem created for small to
+                        midscale businesses, empowering them to compete with
+                        larger players in their respective industries. Our suite
+                        provides essential tools for communication and
+                        technology, offering customizable and highly competent
+                        solutions across all platforms. With a seamless user
+                        experience, JoonMS eliminates many of the challenges
+                        that come with larger manual workforces while boosting
+                        productivity.
+                      </div>
+                      {/* <div className="author">- Raisa Moni</div>
+                      <div className="designation">Envato Customer</div> */}
+                    </div>
+                  </div>
+
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      {/* <div className="quote-icon flaticon-quote-2" /> */}
+                      <div className="text">
+                        Our focus is on user happiness, functional excellence,
+                        long-term thinking, and innovation. We pride ourselves
+                        on making it easy for users to connect with our team,
+                        and this strong customer support has been a key factor
+                        in our clients' success and growth. As we continue to
+                        expand, we aim to provide an all-encompassing solution
+                        that evolves from CaFM basics into a software suite
+                        capable of replacing ERP systems for any business.
+                      </div>
+                      {/* <div className="author">- Raisa Moni</div>
+                      <div className="designation">Envato Customer</div> */}
+                    </div>
+                  </div>
+
+                  <div className="testimonial-block">
+                    <div className="inner-box">
+                      {/* <div className="quote-icon flaticon-quote-2" /> */}
+                      <div className="text">
+                        Managed by a youthful and experienced team from diverse
+                        sectors, JoonMS excels in research and development,
+                        ensuring that we address each necessity smartly and
+                        smoothly. This commitment allows us to stay ahead of the
+                        curve while keeping our users at the heart of everything
+                        we do.
+                      </div>
+                      {/* <div className="author">- Raisa Moni</div>
+                      <div className="designation">Envato Customer</div> */}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
